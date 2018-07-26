6bad4c30568692fbafb043d6c30c2ee725cbaf62
haleykeizur <haleykz@yahoo.com>
2018-07-26 17:13:43 +0000

    Done.

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,39 @@
-#write out your code here
+puts "Welcome to your least coins calculator. Please enter the number of the money you have:"
+cents = gets.strip
+cents = cents.to_i
 
 def least_coins(cents)
+  coins = {
+    :quarters => 0,
+    :dimes => 0,
+    :nickels => 0, 
+    :pennies => 0 
+  }
+  
+# quarters
+while cents >= 25
+  coins[:quarters] += 1 
+  cents = cents - 25
+end 
 
-#Code your answer here!
-
+# dimes
+while cents >= 10 
+  coins[:dimes] += 1 
+  cents = cents - 10 
 end
+
+# nickels
+while cents >= 5 
+  coins[:nickels] += 1 
+  cents = cents - 5 
+end  
+
+# pennies
+while cents >= 1 
+  coins[:pennies] += 1 
+  cents = cents - 1 
+end
+ puts coins 
+end
+puts least_coins(cents)
+
